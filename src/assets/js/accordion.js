97b89e958fce63c13a654a775170d273c3a1f47b
unknown <mikeber000@gmail.com>
2024-04-13 20:54:42 +0800

    testimonials section done, learn how to positionate items on circles using translate with 2 rotates

--- src/assets/js/accordion.js
+++ src/assets/js/accordion.js
@@ -4,29 +4,41 @@ export const accordion = () => {
         
 
         accordions.forEach(accordion => {
+
             const items = accordion.querySelectorAll('.accordion__item');
+
             items.forEach(item => {
-                const arrow = item.querySelector('.arrow');
+                
+                processAccordion(item, 'init');
                 item.addEventListener('click', () => {
-                    
-                    const control = item.querySelector('.accordion__control')
-                    const content = item.querySelector('.accordion__content');
-                    item.classList.toggle('accordion__item_active');
-                    arrow.classList.toggle('arrow_active');
-    
-                    if(item.classList.contains('accordion__item_active')) {
-                        content.style.maxHeight = content.scrollHeight + 'px';
-                        content.setAttribute('aria-hidden', false);
-                        control.setAttribute('aria-expanded', true);
-    
-                    }else {
-                        content.style.maxHeight = 0 + 'px';
-                        content.setAttribute('aria-hidden', true);
-                        control.setAttribute('aria-expanded', false);
-                    }
+                    processAccordion(item);
                 })
             })
         })
+
+        function processAccordion (item, condition='process'){
+            try {
+                const control = item.querySelector('.accordion__control')
+                const content = item.querySelector('.accordion__content');
+                const arrow = item.querySelector('.arrow');
+                if(condition !== 'init') {
+                    item.classList.toggle('accordion__item_active');
+                }
+                if(item.classList.contains('accordion__item_active')) {
+                    content.style.maxHeight = content.scrollHeight + 'px';
+                    content.setAttribute('aria-hidden', false);
+                    control.setAttribute('aria-expanded', true);
+                    arrow.classList.add('arrow_active');
+                }else {
+                    content.style.maxHeight = 0 + 'px';
+                    content.setAttribute('aria-hidden', true);
+                    control.setAttribute('aria-expanded', false);
+                    arrow.classList.remove('arrow_active');
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
         
     } catch (error) {
         console.log(error)
