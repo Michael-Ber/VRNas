ce1e654f67138804512e621ac75242c10c2c9fea
unknown <mikeber000@gmail.com>
2024-04-19 14:51:46 +0800

    service page done

---
+++ src/assets/js/header-scrolling.js
@@ -0,0 +1,14 @@
+export const headerBGAfterScrolling = () => {
+    try {
+        const header = document.querySelector('.header');
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 50) {
+                header.classList.add('header_scrolling')
+            } else {
+                header.classList.remove('header_scrolling')
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,10 +2,12 @@ import { burger } from "./burger.js"
 import { playVideo } from "./video.js";
 import { accordion } from "./accordion.js";
 import { swipers } from "./swipers.js";
+import { headerBGAfterScrolling } from "./header-scrolling.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     burger();
     playVideo();
     accordion();
     swipers();
+    headerBGAfterScrolling();
 })
